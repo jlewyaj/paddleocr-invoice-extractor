e1703c705a9e8995f3b2388e1a6ff7040b26a143
Jaywel Javier <jlewyaj.@gmail.com>
2026-06-30 11:08:23 +0800

    refactor: restructure invoice parser, used function dispatching for value cleaning

--- parser.py
+++ parser.py
@@ -5,54 +5,73 @@ class InvoiceParser:
 
     FIELD_MAP = {
         "receipt_no": [
+            "Official Receipt",
             "Receipt No",
             "Receipt #",
             "OR No",
             "OR #",
-            "Official Receipt",
         ],
-
         "doctor": [
+            "Attending Physician",
+            "Physician",
             "Doctor",
             "Dr.",
-            "Physician",
-            "Attending Physician",
         ],
-
         "patient": [
-            "Patient",
-            "Patient Name",
             "Name of Patient",
+            "Patient Name",
+            "Patient",
             "Client",
         ],
-
         "hospital": [
-            "Hospital",
             "Medical Center",
+            "Hospital",
             "Clinic",
         ],
-
         "date": [
-            "Date",
-            "Receipt Date",
             "Transaction Date",
+            "Receipt Date",
+            "Date",
         ],
-
         "total": [
-            "Total",
             "Total Amount",
             "Grand Total",
             "Amount Due",
+            "Total",
         ]
     }
+
+
+    def __init__(self):
+        self.cleaners = {
+            "total": self.clean_amount,
+            "receipt_no": self.clean_receipt_no,
+        }
+
+    
+    def get_current_and_next_text(self, i, image):
+        current_text = image[i][1][0]
+        if i + 1 < len(image):
+            next_text = image[i+1][1][0]
+        else:
+            next_text = ""
+
+        return current_text, next_text
+
+
+    def match_field(self, current_text):
+        for key, labels in self.FIELD_MAP.items():
+            for label in labels:
+                if label in current_text:
+                    return key, label
+        return None
+
 
     def extract_value(self, current_text, next_text, label):
         value = (
             current_text
             .replace(label, "")
             .replace(":", "")
-            .replace("₱", "")
-            .replace(",", "")
             .strip()
         )
 
@@ -60,30 +79,60 @@ class InvoiceParser:
             value = next_text
         
         return value
+    
+
+    def clean_amount(self, value):
+        return (
+            value
+            .replace("₱", "")
+            .replace(",", "")
+            .strip()
+        )
+    
+    
+    def clean_receipt_no(self, value):
+        return (
+            value
+            .replace("No.", "")
+            .replace("No", "")
+            .strip()
+        )
+    
+    
+    def clean_value(self, key, value):
+
+        cleaner = self.cleaners.get(key)
+
+        if cleaner:
+            value = cleaner(value)
+
+        return value
+
 
     def parse(self, result):
         invoice = Invoice()
 
         for image in result:
-            for i, box in enumerate(image):
+            for i, _ in enumerate(image):
+                
+                current_text, next_text = self.get_current_and_next_text(i, image)
 
-                current_text = box[1][0]
+                field = self.match_field(current_text)
+                
+                if field:
+                    key, label = field
+                
+                    value = self.extract_value(
+                        current_text,
+                        next_text,
+                        label
+                    )
 
-                if i + 1 < len(image):
-                    next_text = image[i+1][1][0]
-                else:
-                    next_text = ""
+                    value = self.clean_value(
+                        key,
+                        value
+                    )
 
-                for key, labels in self.FIELD_MAP.items():
-                    for label in labels:
-                        if label in current_text: 
-                            value = self.extract_value(
-                                current_text,
-                                next_text,
-                                label
-                            )
-                            setattr(invoice, key, value)
+                    setattr(invoice, key, value)
 
-                            break
-        
         return invoice
